3516037afb341702cc088342c61dbf04b6d2106b
Renzo-skudize <150698694+Renzo-skudize@users.noreply.github.com>
2024-11-10 14:14:22 +0100

    Create AeneasAchilles.js

---
+++ gui/pregame/backgrounds/AeneasAchilles.js
@@ -0,0 +1,18 @@
+g_BackgroundLayerData.push(
+  [
+    {
+      "offset": (time, width) => 0,
+      "sprite": "AeneasAchilles",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "AeneasAchilles",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "AeneasAchilles",
+      "tiling": false,
+    },
+  ]);
