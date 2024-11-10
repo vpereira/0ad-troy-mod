754dae4208f9d4ab942f2c8c1a690e1c410c3a7c
Renzo-skudize <150698694+Renzo-skudize@users.noreply.github.com>
2024-11-10 15:49:56 +0100

    Create GreekArrival.js

---
+++ gui/pregame/backgrounds/GreekArrival.js
@@ -0,0 +1,18 @@
+g_BackgroundLayerData.push(
+  [
+    {
+      "offset": (time, width) => 0,
+      "sprite": "Home",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "Home",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "Home",
+      "tiling": false,
+    },
+  ]);
