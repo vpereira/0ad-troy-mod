26fae64086b940bb3706b4ecde3cb3c6f340e14b
Renzo-skudize <150698694+Renzo-skudize@users.noreply.github.com>
2024-11-10 13:11:34 +0100

    Update Battle_of_the_ships.js

--- gui/pregame/backgrounds/Battle_of_the_ships.js
+++ gui/pregame/backgrounds/Battle_of_the_ships.js
@@ -1,1 +1,18 @@
-
+g_BackgroundLayerData.push(
+  [
+    {
+      "offset": (time, width) => 0,
+      "sprite": "Battle_of_the_ships",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "Battle_of_the_ships",
+      "tiling": false,
+    },
+    {
+      "offset": (time, width) => 0,
+      "sprite": "Battle_of_the_ships",
+      "tiling": false,
+    },
+  ]);
